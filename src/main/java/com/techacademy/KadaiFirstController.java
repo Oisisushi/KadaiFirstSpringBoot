7d49242f2a58920949a99b969e137fb6b4e469ee
Oisisushi <osushi.oisisushi@gmail.com>
2024-02-01 09:02:32 +0900

    dispDayOfWeekメソッドの中身を作成
    CalendarクラスのDAY_OF_WEEKフィールドで曜日の情報を取得
    if文で表示用文字列を代入してreturnする形で作成

--- src/main/java/com/techacademy/KadaiFirstController.java
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -1,5 +1,7 @@
 package com.techacademy;
 
+import java.util.Calendar;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,14 +9,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KadaiFirstController {
 
-    @GetMapping("dayofweek")
-    public String dispDayOfWeek(){
-        //仕様--URLyyyymmddで日付を指定する、実行して曜日を返す
+    @GetMapping("dayofweek/{day}")
+    public String dispDayOfWeek(@PathVariable String day){
+        int dWeek = 0;
+        String week = null;
+        Calendar cal = Calendar.getInstance();
+        cal.set(Integer.parseInt(day.substring(0,4)),Integer.parseInt(day.substring(4,6)),Integer.parseInt(day.substring(6,8)));
+        dWeek = cal.get(Calendar.DAY_OF_WEEK);
+        if (dWeek == 1) {
+            week = "Sunday";
+        } else if (dWeek == 2) {
+            week = "Monday";
+        } else if (dWeek == 3) {
+            week = "Tuesday";
+        } else if (dWeek == 4) {
+            week = "Wednesday";
+        } else if (dWeek == 5) {
+            week = "Thursday";
+        } else if (dWeek == 6) {
+            week = "Friday";
+        } else if (dWeek ==7) {
+            week = "Saturday";
+        }
 
-        /*まずURLを変数にして年、月、日に変換する
-          暦から曜日を求める
-          取得した曜日を表示に適したかたちに直す*/
-        return "ここに曜日を出力させる";
+        return week;
     }
 
     //ここから四則演算メソッド
